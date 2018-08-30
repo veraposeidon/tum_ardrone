ff13af82af6b84dfc4620bd0626537ff89d72a9d
veraposeidon <veraposeidon@gmail.com>
2018-08-30 21:50:28 +0800

    了解了stateestimation节点的主程序框架，需要继续了解卡尔曼滤波器和PTAM的细节

--- src/stateestimation/main_stateestimation.cpp
+++ src/stateestimation/main_stateestimation.cpp
@@ -19,6 +19,8 @@
  */
  
 #include "EstimationNode.h"
+// 这个头文件在EstimationNode中，由配置文件所生成。
+//#include "tum_ardrone/StateestimationParamsConfig.h"
 #include "ros/ros.h"
 #include "PTAMWrapper.h"
 #include "MapView.h"
@@ -31,25 +33,35 @@ unsigned int ros_header_timestamp_base = 0;
 
 int main(int argc, char **argv)
 {
+    // 初始化drone_stateestimation节点
+    ros::init(argc, argv, "drone_stateestimation");
 
-  ros::init(argc, argv, "drone_stateestimation");
+    ROS_INFO("Started TUM ArDrone Stateestimation Node.");
 
-  ROS_INFO("Started TUM ArDrone Stateestimation Node.");
+    // 建立一个姿态估计 对象
+    EstimationNode estimator;
 
-  EstimationNode estimator;
+    // 创建了一个参数动态配置的服务端实例，参数配置的类型就是配置文件中描述的类型
+    // 该服务端实例会监听客户端的参数配置请求。
+    dynamic_reconfigure::Server<tum_ardrone::StateestimationParamsConfig> srv;
 
-  dynamic_reconfigure::Server<tum_ardrone::StateestimationParamsConfig> srv;
-  dynamic_reconfigure::Server<tum_ardrone::StateestimationParamsConfig>::CallbackType f;
-  f = boost::bind(&EstimationNode::dynConfCb, &estimator, _1, _2);
-  srv.setCallback(f);
+    // 定义一个回调函数，并将回调函数和服务端绑定，当客户端请求修改参数时，服务端即可跳转到回调函数中进行处理。
+    dynamic_reconfigure::Server<tum_ardrone::StateestimationParamsConfig>::CallbackType f;
+    f = boost::bind(&EstimationNode::dynConfCb, &estimator, _1, _2);
+    srv.setCallback(f);
 
-  estimator.ptamWrapper->startSystem();
-  estimator.mapView->startSystem();
+    // 启动PTAM封装器
+    estimator.ptamWrapper->startSystem();
+    // 启动位姿图显示
+    estimator.mapView->startSystem();
 
-  estimator.Loop();
+    // main pose-estimation loop
+    estimator.Loop();
 
-  estimator.mapView->stopSystem();
-  estimator.ptamWrapper->stopSystem();
+    // 关闭
+    estimator.mapView->stopSystem();
+    estimator.ptamWrapper->stopSystem();
 
-  return 0;
+    return 0;
 }
+
